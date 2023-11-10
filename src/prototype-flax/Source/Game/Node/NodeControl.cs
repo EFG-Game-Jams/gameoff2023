e967766e020053cafd9489a0de8d25a88ffb33d9
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2023-11-10 12:41:19 +0100

    Add prototype-flax

---
+++ src/prototype-flax/Source/Game/Node/NodeControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// NodeControl Script.
+/// </summary>
+public class NodeControl : Script
+{
+    public NodeFlowType Type;
+
+    public Node Node;
+
+    public SpriteRender Sprite;
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        // Here you can add code that needs to be called when script is created, just before the first game update
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        // Here you can add code that needs to be called every frame
+    }
+}
---
+++ src/prototype-flax/Source/Game/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Player Script.
+/// </summary>
+public class Player : Script
+{
+    public Camera Camera;
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        // Here you can add code that needs to be called when script is created, just before the first game update
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        if (Input.GetMouseButtonDown(MouseButton.Left))
+        {
+            var pos = Input.MousePosition;
+            var ray = Camera.MainCamera.ConvertMouseToRay(pos);
+            foreach (var node in Scene.GetChild("Nodes").Children)
+            {
+                foreach (var control in node.GetChild("PlayerControls").GetChildren<SpriteRender>())
+                {
+                    if (ray.Intersects(new BoundingSphere(control.Position, control.Size.X / 2f)))
+                    {
+                        var nodeModel = node.GetChild("SphereCollider").GetChild<StaticModel>();
+                        node.GetScript<Node>().OnFlowTypeChange(control.GetScript<NodeControl>().Type);
+                        nodeModel.GetMaterial(0).SetParameterValue("Color", control.Color);
+                    }
+                }
+            }
+        }
+        // Here you can add code that needs to be called every frame
+    }
+}
