3f057b0bbe59cdf43421ac53a61b75633c00b82f
SatriAli <SatriAli@users.noreply.github.com>
2023-11-25 16:54:26 +0100

    add actor quadtree and test script
    this can soon be used for the asteroid spawn-config points

---
+++ src/prototype-flax/Source/SCALE/QuadTree/QuadTree.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using FlaxEngine;
+
+public class Quadtree : Script
+{
+	private class Node
+	{
+		public Rectangle Bounds;
+		public List<Actor> Items;
+		public Node[] Children;
+
+		public Node(Rectangle bounds)
+		{
+			Bounds = bounds;
+			Items = new();
+			Children = null;
+		}
+
+		public bool IsLeaf => Children == null;
+	}
+
+	private Node root;
+
+	public Rectangle Bounds
+	{
+		get => root.Bounds;
+		set => root = new Node(value);
+	}
+	public int MaxItemsPerNode { get; set; } = 4;
+	
+	public Quadtree()
+	{
+	}
+
+	public void Insert(Actor item)
+	{
+		Insert(item, root);
+	}
+
+	private void Insert(Actor item, Node node)
+	{
+		Vector2 position = (Vector2)item.Position;
+
+		// If the probe is not within this node, return
+		if (!node.Bounds.Contains(position))
+			return;
+
+		if (node.IsLeaf)
+		{
+			// Add the probe to the node
+			node.Items.Add(item);
+
+			// Split the node if needed
+			if (node.Items.Count > MaxItemsPerNode)
+				Split(node);
+		}
+		else
+		{
+			// Insert the probe into the appropriate child
+			foreach (var child in node.Children)
+				Insert(item, child);
+		}
+	}
+
+	private void Split(Node node)
+	{
+		float halfWidth = node.Bounds.Width * 0.5f;
+		float halfHeight = node.Bounds.Height * 0.5f;
+		float x = node.Bounds.X;
+		float y = node.Bounds.Y;
+
+		// Creating four child nodes
+		node.Children = new Node[4];
+		node.Children[0] = new Node(new Rectangle(x, y, halfWidth, halfHeight)); // Top left
+		node.Children[1] = new Node(new Rectangle(x + halfWidth, y, halfWidth, halfHeight)); // Top right
+		node.Children[2] = new Node(new Rectangle(x, y + halfHeight, halfWidth, halfHeight)); // Bottom left
+		node.Children[3] = new Node(new Rectangle(x + halfWidth, y + halfHeight, halfWidth, halfHeight)); // Bottom right
+
+		// Reassign items to children
+		foreach (Actor item in node.Items)
+		{
+			Vector2 position = (Vector2)item.Position;
+			foreach (var child in node.Children)
+			{
+				if (child.Bounds.Contains(position))
+				{
+					child.Items.Add(item);
+					break;
+				}
+			}
+		}
+
+		// Clear items from the current node after reassigning
+		node.Items.Clear();
+	}
+
+	private void Query(Rectangle area, List<Actor> result)
+	{
+		Query(area, root, result);
+	}
+	public List<Actor> Query(Rectangle area)
+	{
+		var result = new List<Actor>();
+		Query(area, result);
+		return result;
+	}	
+	private void Query(Rectangle area, Node node, List<Actor> result)
+	{
+		if (!node.Bounds.Intersects(ref area))
+			return;
+
+		if (node.IsLeaf)
+		{
+			foreach (var probe in node.Items)
+			{
+				Vector2 position = (Vector2)probe.Position;
+				if (area.Contains(position))
+					result.Add(probe);
+			}
+		}
+		else
+		{
+			foreach (var child in node.Children)
+				Query(area, child, result);
+		}
+	}
+
+	public List<Rectangle> QueryCells(Rectangle area, bool leafNodesOnly)
+	{
+		var result = new List<Rectangle>();
+		QueryCells(area, leafNodesOnly, result);
+		return result;
+	}
+	public void QueryCells(Rectangle area, bool leafNodesOnly, List<Rectangle> result)
+	{
+		QueryCells(area, root, leafNodesOnly, result);
+	}
+	private void QueryCells(Rectangle area, Node node, bool leafNodesOnly, List<Rectangle> result)
+	{
+		if (!node.Bounds.Intersects(ref area))
+			return;
+
+		if (node.IsLeaf)
+		{
+			result.Add(node.Bounds);
+		}
+		else
+		{
+			if (!leafNodesOnly)
+				result.Add(node.Bounds);
+			foreach (var child in node.Children)
+				QueryCells(area, child, leafNodesOnly, result);
+		}
+	}
+}
---
+++ src/prototype-flax/Source/SCALE/QuadTree/QuadTreeTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace SCALE;
+
+/// <summary>
+/// QuadTreeTest Script.
+/// </summary>
+public class QuadTreeTest : Script
+{
+    public int itemCount = 1024;
+    public Rectangle bounds = new Rectangle(-1000, -1000, 2000, 2000);
+
+    public Actor samplePoint;
+    public float sampleRadius = 5000f;
+    public bool sampleUsingBruteForce;
+
+    public float lastQueryTimeMs;
+
+    private Quadtree tree;
+    private List<EmptyActor> items;
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        tree = Actor.AddScript<Quadtree>();
+        tree.Bounds = bounds;
+        tree.MaxItemsPerNode = 16;
+
+        items = new();
+		for (int i = 0; i < itemCount; i++)
+        {
+			EmptyActor actor = new EmptyActor();
+			actor.Position = new Vector3(Random.Shared.NextSingle() * bounds.Width + bounds.X, Random.Shared.NextSingle() * bounds.Height + bounds.Y, 0);
+			tree.Insert(actor);
+            items.Add(actor);
+		}
+    }
+
+	public override void OnDebugDraw()
+	{
+        if (Time.GamePaused)
+            return;
+		//foreach (var item in items)
+		//	DebugDraw.DrawCircle(item.Position, Float3.Backward, 50f, Color.Green);
+	}
+
+	/// <inheritdoc/>
+	public override void OnUpdate()
+    {
+        if (samplePoint == null)
+			return;
+
+        Rectangle rect = new Rectangle(samplePoint.Position.X - sampleRadius, samplePoint.Position.Y - sampleRadius, sampleRadius * 2, sampleRadius * 2);
+        
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        List<Actor> items = null;
+        if (sampleUsingBruteForce)
+        {
+            items = new List<Actor>();
+            float d2 = sampleRadius * sampleRadius;
+            foreach (var actor in this.items)
+            {
+                if (Float3.DistanceSquared(actor.Position, samplePoint.Position) <= d2)
+                    items.Add(actor);
+            }
+        }
+        else
+        {
+            items = tree.Query(rect);
+        }
+        lastQueryTimeMs = (float)stopwatch.Elapsed.TotalMilliseconds;
+
+        foreach (var item in items)
+        {
+            if (Float3.Distance(item.Position, samplePoint.Position) <= sampleRadius)
+    			DebugDraw.DrawCircle(item.Position, Float3.Backward, 5f, Color.Green);
+            else
+    			DebugDraw.DrawCircle(item.Position, Float3.Backward, 5f, Color.Red);
+			// draw a line to the sample point
+			//DebugDraw.DrawLine(item.Position, samplePoint.Position, Color.Blue);
+		}
+
+        var cells = tree.QueryCells(rect, false);
+        foreach (var cell in cells)
+        {
+            // draw a rectangle around the cell
+            var r = cell;
+            /*r.X += 10;
+            r.Y += 10;
+            r.Width -= 20;
+            r.Height -= 20;*/
+            DebugDraw.DrawWireBox(new BoundingBox((Float3)(r.UpperLeft), (Float3)(r.BottomRight)), Color.Gray);
+		}
+    }
+}
