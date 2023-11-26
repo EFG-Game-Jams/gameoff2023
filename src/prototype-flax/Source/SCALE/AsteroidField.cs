ecb0393894c05bb49413ad5b5c465d0f08541100
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2023-11-26 18:00:04 +0100

    Improve asteroid field spawning algorithm

--- src/prototype-flax/Source/SCALE/AsteroidField.cs
+++ src/prototype-flax/Source/SCALE/AsteroidField.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using FlaxEditor.Content.Settings;
-using FlaxEditor.Surface.Archetypes;
 using FlaxEngine;
 using Material = FlaxEngine.Material;
 
@@ -13,13 +11,14 @@ namespace SCALE;
 /// </summary>
 public class AsteroidField : Script
 {
-	public Actor Player;
+	public RigidBody Player;
+	public Camera Camera;
+
 	[CustomEditorAlias("FlaxEditor.CustomEditors.Editors.ActorLayerEditor")]
 	public int AsteroidLayer;
-	public float killDistance = 10000;
-	public int maxAsteroids = 10;
+	public double AsteroidDespawnRadius = 10_000.0;
+	public int MaxAsteroids = 25;
 	public Float2 asteroidScale = new Float2(1, 10);
-	public Float2 asteroidSpawnDistance = new Float2(1000, 10000);
 	public Material asteroidMaterial;
 
 	private HashSet<Actor> asteroids = new HashSet<Actor>();
@@ -32,47 +31,115 @@ public class AsteroidField : Script
 			Debug.Log($"Physics origin: {Physics.DefaultScene.Origin}");
 		}
 
-		// kill
+		DespawnAsteroidsTooFarAway();
+
+		var toSpawnAsteroidCount = Math.Max(0, MaxAsteroids - asteroids.Count);
+		SpawnNewAsteroids(
+			toSpawnAsteroidCount,
+			allowSpawnInView: asteroids.Count == 0);
+	}
+
+	private void DespawnAsteroidsTooFarAway()
+	{
 		foreach (var asteroid in asteroids.ToList())
 		{
-			if (Vector3.Distance(asteroid.Position, Player.Position) > killDistance)
+			if (Vector3.Distance(asteroid.Position, Player.Position) > AsteroidDespawnRadius)
 			{
 				asteroids.Remove(asteroid);
 				Destroy(asteroid);
 			}
 		}
+	}
 
-		// spawn
+	private void SpawnNewAsteroids(int count, bool allowSpawnInView)
+	{
+		const int sampleLimit = 64;
 		int sampleCount = 0;
-		while (asteroids.Count < maxAsteroids && sampleCount < 64)
+
+		float cameraDepth = (float)(-1 * Camera.Position.Z);
+		var fustrumWidthPlayerPlane = Camera.Frustum.GetWidthAtDepth(cameraDepth);
+		var fustrumHeightPlayerPlane = Camera.Frustum.GetHeightAtDepth(cameraDepth);
+
+		var normalizedPlayerDirection = Player.LinearVelocity.Normalized;
+		var minimumSpawnRadius = 0.0;
+		if (!allowSpawnInView)
 		{
-			float angle = (RandomUtil.Random.NextSingle() * 2f - 1f) * Mathf.TwoPi;
-			Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
-			Vector3 position = Player.Position + direction * (RandomUtil.Random.NextSingle() * (asteroidSpawnDistance.Y - asteroidSpawnDistance.X) + asteroidSpawnDistance.X);
-			float scale = RandomUtil.Random.NextSingle() * (asteroidScale.Y - asteroidScale.X) + asteroidScale.X;
+			minimumSpawnRadius = Math.Sqrt(fustrumWidthPlayerPlane * fustrumWidthPlayerPlane + fustrumHeightPlayerPlane * fustrumHeightPlayerPlane) / 2.0;
+		}
+		var maximumSpawnRadius = AsteroidDespawnRadius - 100;
+		if (minimumSpawnRadius >= maximumSpawnRadius)
+		{
+			Debug.LogError($"The minimum asteroid spawn distance {minimumSpawnRadius} is greater or equal to the maximum asteroid spawn distance {maximumSpawnRadius}");
+			return;
+		}
 
-			++sampleCount;
-			if (Physics.CheckSphere(position, scale * 100))
-				continue;
-
+		for (int i = 0; i < count; ++i)
+		{
 			var proceduralAsteroids = Parent.GetScript<ProceduralAsteroids>();
 			var variant = proceduralAsteroids.GetRandomVariant();
+			var asteroidBoundingBox = variant.Model.GetBox();
+			var maximumBoundingBoxRadius = Math.Max(Math.Max(asteroidBoundingBox.Size.X, asteroidBoundingBox.Size.Y), asteroidBoundingBox.Size.Z) / 2.0;
 
-			var asteroid = Parent.AddChild<StaticModel>();
-			asteroid.Model = variant.Model;
-			asteroid.Position = position;
-			asteroid.EulerAngles = new Float3(0, 0, Random.Shared.NextSingle() * 360);
-			asteroid.SetMaterial(0, asteroidMaterial);
-			//asteroid.Scale = Vector3.One * scale;
+			Vector3 randomDirection;
+			if (normalizedPlayerDirection.X + normalizedPlayerDirection.Y + normalizedPlayerDirection.Z < 0.1)
+			{
+				// The player is barely moving, spawn stuff anywhere in a circle around the player
+				var randomAngle = (RandomUtil.Random.NextSingle() * 2.0 - 1.0) * Mathd.TwoPi;
+				randomDirection = new Vector3(Mathd.Cos(randomAngle), Mathd.Sin(randomAngle), 0);
+			}
+			else
+			{
+				// The player is moving, only spawn in the general direction of the movement
+				var ninetyDegrees = Mathd.Pi / 2.0;
+				var randomAngleOffset = RandomUtil.Rand() * ninetyDegrees - (ninetyDegrees / 2.0); // Some angle between -45 and 45 degrees
 
-			var meshCollider = asteroid.AddChild<MeshCollider>();
-			meshCollider.CollisionData = variant.CollisionData;
-			meshCollider.Layer = AsteroidLayer;
+				var playerDirectionAsAngle = Vector3.Angle(normalizedPlayerDirection, new Vector3(0, 1, 0)) * Mathd.DegreesToRadians;
+				var randomAngle = randomAngleOffset + playerDirectionAsAngle;
 
-			var asteroidTrigger = meshCollider.AddScript<AsteroidTrigger>();
-			asteroidTrigger.Player = Player;
+				randomDirection = new Vector3(Mathd.Cos(randomAngle), Mathd.Sin(randomAngle), 0);
+			}
 
-			asteroids.Add(asteroid);
+			while (true)
+			{
+				if (sampleCount >= sampleLimit)
+				{
+					Debug.LogWarning("Reached sample limit for spawning asteroids");
+					return;
+				}
+				++sampleCount;
+
+				var offset = randomDirection * (minimumSpawnRadius + RandomUtil.Rand() * (maximumSpawnRadius - minimumSpawnRadius));
+				Vector3 candidatePosition = Player.Position + offset;
+
+				if (Physics.CheckSphere(
+					candidatePosition,
+					(float)maximumBoundingBoxRadius,
+					hitTriggers: true))
+				{
+					continue;
+				}
+
+				var asteroid = Parent.AddChild<StaticModel>();
+				asteroid.Model = variant.Model;
+				asteroid.Position = candidatePosition;
+				asteroid.EulerAngles = new Float3(0, 0, Random.Shared.NextSingle() * 360);
+				asteroid.SetMaterial(0, asteroidMaterial);
+
+				var meshCollider = asteroid.AddChild<MeshCollider>();
+				meshCollider.CollisionData = variant.CollisionData;
+				meshCollider.Layer = AsteroidLayer;
+
+				var asteroidTrigger = meshCollider.AddScript<AsteroidTrigger>();
+				asteroidTrigger.Player = Player;
+
+				asteroids.Add(asteroid);
+				break;
+			}
 		}
+	}
+
+	private Vector3 GetRandomAsteroidSpawnPosition(BoundingSphere boundingSphere)
+	{
+		throw new NotImplementedException();
 	}
 }
